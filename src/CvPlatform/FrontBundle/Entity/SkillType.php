9490b3c45a1697e834c79a66ad5ac9f0342d1039
mhor <maxime.horcholle@gmail.com>
2014-01-14 14:46:02 +0100

    implement mapped super class

---
+++ src/CvPlatform/FrontBundle/Entity/SkillType.php
@@ -0,0 +1,19 @@
+<?php
+namespace CvPlatform\FrontBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Mhor\CvToPdfBundle\Entity\SkillType as BaseSkillType;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="skill_type")
+ */
+class SkillType extends BaseSkillType
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
